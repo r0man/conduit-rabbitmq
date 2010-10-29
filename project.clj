2a56fe1ae4b6843b52af483100950819b1061ecd
Tim Dysinger <tim@dysinger.net>
2010-10-29 10:39:00 -0400

    Shortened description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-  :description "conduit-rabbitmq: 
-               Build distributed applications in Clojure using RabbitMQ"
 (defproject conduit-rabbitmq "0.7.0-SNAPSHOT"
+  :description "Conduit Rabbitmq: Conduit Transport for RabbitMQ"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.rabbitmq/amqp-client "1.7.2"]
