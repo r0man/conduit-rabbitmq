9489bd257aa138415bfc966991be8a77fc3517bc
Tim Dysinger <tim@dysinger.net>
2010-10-29 10:35:37 -0400

    Project name change

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject net.intensivesystems/conduit-rabbitmq "0.7.0-SNAPSHOT"
   :description "conduit-rabbitmq: 
                Build distributed applications in Clojure using RabbitMQ"
+(defproject conduit-rabbitmq "0.7.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.rabbitmq/amqp-client "1.7.2"]
-                 [net.intensivesystems/arrows "1.2.0"]
-                 [net.intensivesystems/conduit "0.7.0-SNAPSHOT"]])
+                 [arrows "1.2.0"]
+                 [conduit "0.7.0-SNAPSHOT"]])
