bc1e17206642eb53fbdb4e0586ba87fc2962d339
Joe Gallo <joegallo@gmail.com>
2010-11-09 15:06:29 -0500

    Add lambda repository.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.rabbitmq/amqp-client "1.7.2"]
-                 [conduit "0.8.0"]])
+                 [conduit "0.8.0"]]
+  :repositories {"lambda" "http://lambda.sa2s.us/snapshots/"})
