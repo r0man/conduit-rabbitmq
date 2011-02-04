45370d0df2e0a7307c624494577b364c7468b485
Jim Duey <jim.duey@sonian.net>
2011-02-04 12:58:18 -0800

    Bumped version to 0.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conduit-rabbitmq "0.8.0"
+(defproject conduit-rabbitmq "0.8.1"
   :description "Conduit Rabbitmq: Conduit Transport for RabbitMQ"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
