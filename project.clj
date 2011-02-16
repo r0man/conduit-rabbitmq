fde89bc7f25f856c7b5feb1986d92bde2288374e
Joe Gallo <joegallo@gmail.com>
2011-02-16 18:37:18 -0500

    Upgrade to rabbitmq 2.3.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject conduit-rabbitmq "0.8.1"
+(defproject conduit-rabbitmq "0.9.0"
   :description "Conduit Rabbitmq: Conduit Transport for RabbitMQ"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.rabbitmq/amqp-client "1.7.2"]
+                 [com.rabbitmq/amqp-client "2.3.1"]
                  [conduit "0.8.0"]]
   :repositories {"lambda" "http://lambda.sa2s.us/snapshots/"})
