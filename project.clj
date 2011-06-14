0fc1a4e13b7d552c3953061a80b3f5cd6e55f38d
Phil Hagelberg <technomancy@gmail.com>
2011-06-14 15:02:23 -0700

    Add :repositories to project.clj for ci purposes.

--- project.clj
+++ project.clj
@@ -3,12 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.rabbitmq/amqp-client "2.3.1"]
-                 [conduit "0.8.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [org.clojars.technomancy/clj-stacktrace
-                      "0.2.1-20101126.163600-6"]
-                     [lein-difftest "1.3.2-20101010.033133-1"
-                      :exclusions [clj-stacktrace]]
-                     [lein-release "1.1.1"]
-                     [lein-fail-fast "1.0.0"]]
-  :repositories {"lambda" "http://lambda.sa2s.us/snapshots/"})
+                 [conduit "0.8.5"]]
+  ;; for ci purposes
+  :repositories {"snapshots" "http://localhost/archiva/repository/snapshots"
+                 "releases" "http://localhost/archiva/repository/internal"})
